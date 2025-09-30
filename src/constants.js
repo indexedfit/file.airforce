34b4bfede0d9da30cf77107e1c3c467861b77d95
indexedfit <223321476+indexedfit@users.noreply.github.com>
2025-09-30 21:03:06 +0300

    cleanup

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const PUBSUB_PEER_DISCOVERY = "gayboys-inc";
 // need to autogenerate some local fellows for good E2E testing!
 // TODO: that.
 export const TRACKERS = [
-  "/ip4/127.0.0.1/tcp/9001/ws/p2p/12D3KooWMNECe2FshMRjhMSWCFC7cNvwHw9Thvpbsv5rJwD9JZpV",
+  "/ip4/127.0.0.1/tcp/9001/ws/p2p/12D3KooWLPmqzEcgRUX3mpb45mxZcGD3f7QpoJUiKUqNNMBZW1C4",
   // public ones on ipfs.fyi/utils go here:
   // todo: cool DNS piggybacking and vibes.
 ];
